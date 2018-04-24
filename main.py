b65cd26fa179fca23f490eb634d9143a63f0511f
Dion Ikink <ikinkdion@gmail.com>
2018-04-24 17:21:40 +0200

    Working Snake game

--- main.py
+++ main.py
@@ -1,91 +1,98 @@
 import pyglet
-from pyglet.window import BaseWindow
 import pyglet.window.key as key
-import pyglet.window.mouse as mouse
+from pyglet.window import Window
+
+from snake import Snake
+
+LEFT = key.LEFT
+RIGHT = key.RIGHT
+UP = key.UP
+DOWN = key.DOWN
+SPACE = key.SPACE
+RESET = key.R
+FRAME = key.ENTER
+
+WIDTH = 720
+HEIGHT = 720
+CELL_SIZE = 20
+FRAME_RATE = 1 / 12
+
+window = Window(WIDTH, HEIGHT)
+snake = Snake(WIDTH, HEIGHT, CELL_SIZE)
+running = False
+game_over = False
+fps_display = pyglet.clock.ClockDisplay()
+pyglet.gl.glClearColor(255, 255, 255, 255)
+label_paused = pyglet.text.Label("PAUSED",
+                                 font_size=50,
+                                 color=(0, 0, 0, 255),
+                                 x=CELL_SIZE, y=CELL_SIZE,
+                                 anchor_x='left', anchor_y='bottom')
+label_game_over = pyglet.text.Label("GAME OVER",
+                                    font_size=50,
+                                    color=(255, 0, 0, 255),
+                                    x=WIDTH / 2, y=HEIGHT / 2,
+                                    anchor_x='center', anchor_y='center')
+label_restart = pyglet.text.Label("Press R to restart",
+                                  font_size=30,
+                                  color=(0, 0, 0, 255),
+                                  x=WIDTH / 2, y=(HEIGHT / 2) - 50,
+                                  anchor_x='center', anchor_y='center')
+label_score = pyglet.text.Label(text="Score: 0", x=20, y=5, color=(255, 255, 255, 255))
 
 
-class SnakeWindow(BaseWindow):
-    def __init__(self, width, height, cell_size, frame_rate, title):
-        super().__init__(width, height, title)
-        self.cell_size = cell_size
-        self.frame_rate = frame_rate
-        self.snake = Snake(width, height, cell_size)
-        self.running = False
-        self.fps_display = pyglet.clock.ClockDisplay()
-        pyglet.gl.glClearColor(255, 255, 255, 255)
+@window.event
+def on_draw():
+    window.clear()
+    snake.draw()
+    snake.draw_grid()
+    label_score.text = "Score: {}".format(snake.score)
+    label_score.draw()
 
-        self.label_paused = pyglet.text.Label("PAUSED",
-                                              font_size=50,
-                                              color=(0, 0, 0, 255),
-                                              x=self.cell_size, y=self.cell_size,
-                                              anchor_x='left', anchor_y='bottom')
+    if not running:
+        label_paused.draw()
 
-    def on_draw(self):
-        self.clear()
-        self.game_of_life.draw()
-        self.game_of_life.draw_grid()
+    if game_over:
+        label_game_over.draw()
+        label_restart.draw()
+        pyglet.clock.unschedule(update)
 
-        if not self.running:
-            self.label_paused.draw()
+    fps_display.draw()
 
-        self.fps_display.draw()
 
-    def on_key_press(self, symbol, modifiers):
-        if symbol == key.SPACE and self.running:
-            pyglet.clock.unschedule(self.update)
-            self.running = False
-        elif symbol == key.SPACE and not self.running:
-            pyglet.clock.schedule_interval(self.update, self.frame_rate)
-            self.running = True
-        elif symbol == key.RIGHT and not self.running:
-            self.game_of_life.run_rules()
-        elif symbol == key.C:
-            self.game_of_life.clear()
-        elif symbol == key.F:
-            self.game_of_life.fill()
-        elif symbol == key.R:
-            self.game_of_life.randomize()
+@window.event
+def on_key_press(symbol, modifiers):
+    global game_over
+    global running
+    global FRAME_RATE
 
-    def on_mouse_press(self, x, y, button, modifiers):
-        if button == mouse.LEFT:
-            row = int(y / self.cell_size)
-            col = int(x / self.cell_size)
+    if symbol == SPACE and running and not game_over:
+        pyglet.clock.unschedule(update)
+        running = False
+    elif symbol == SPACE and not running and not game_over:
+        pyglet.clock.schedule_interval(update, FRAME_RATE)
+        running = True
+    elif symbol == LEFT:
+        snake.set_direction("LEFT")
+    elif symbol == RIGHT:
+        snake.set_direction("RIGHT")
+    elif symbol == UP:
+        snake.set_direction("UP")
+    elif symbol == DOWN:
+        snake.set_direction("DOWN")
+    elif symbol == RESET:
+        snake.reset()
+        running = False
+        game_over = False
+        pyglet.clock.unschedule(update)
+    elif symbol == FRAME:
+        snake.run_rules()
 
-            self.game_of_life.fill_cell(row, col)
 
-        if button == mouse.RIGHT:
-            row = int(y / self.cell_size)
-            col = int(x / self.cell_size)
-
-            self.game_of_life.empty_cell(row, col)
-
-    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
-        if buttons == mouse.LEFT:
-            row = int(y / self.cell_size)
-            col = int(x / self.cell_size)
-
-            self.game_of_life.fill_cell(row, col)
-
-        if buttons == mouse.RIGHT:
-            row = int(y / self.cell_size)
-            col = int(x / self.cell_size)
-
-            self.game_of_life.empty_cell(row, col)
-
-    def update(self, dt):
-        self.game_of_life.run_rules()
-
-class HelloWorldWindow(pyglet.window.Window):
-    def __init__(self):
-        super(HelloWorldWindow, self).__init__()
-
-        self.label = pyglet.text.Label('Hello, world!')
-
-    def on_draw(self):
-        self.clear()
-        self.label.draw()
+def update(dt):
+    global game_over
+    game_over = snake.run_rules()
 
 
 if __name__ == '__main__':
-    window = Window(1280, 720, 20, 1 / 12, "Game of Life")
     pyglet.app.run()
--- snake.py
+++ snake.py
@@ -1,1 +1,173 @@
+import random
+
+import pyglet
+
+DIRECTIONS = {"LEFT": [-1, 0],
+              "RIGHT": [1, 0],
+              "UP": [0, 1],
+              "DOWN": [0, -1]}
+
+
 class Snake:
+    def __init__(self, window_width, window_height, cell_size):
+        self.grid_width = int(window_width / cell_size)
+        self.grid_height = int(window_height / cell_size)
+        self.cell_size = cell_size
+        self.head = (int(self.grid_width / 2), int(self.grid_height / 2))
+        self.snake = [(int(self.grid_width / 2), int(self.grid_height / 2) - 1),
+                      (int(self.grid_width / 2), int(self.grid_height / 2) - 2),
+                      (int(self.grid_width / 2), int(self.grid_height / 2) - 3)]
+        self.direction = "UP"
+
+        self.food = self.generate_coords()
+        self.updated = True
+
+        self.score = 0
+
+        pyglet.gl.glClearColor(255, 255, 255, 255)
+
+    @property
+    def head_x(self):
+        return self.head[0]
+
+    @property
+    def head_y(self):
+        return self.head[1]
+
+    @property
+    def food_x(self):
+        return self.food[0]
+
+    @property
+    def food_y(self):
+        return self.food[1]
+
+    def draw_grid(self):
+        main_batch = pyglet.graphics.Batch()
+
+        for row in range(self.grid_height):
+            line_coords = [0, row * self.cell_size,
+                           self.grid_width * self.cell_size, row * self.cell_size]
+
+            main_batch.add(2, pyglet.gl.GL_LINES, None,
+                           ('v2i', line_coords),
+                           ('c3B', [0, 0, 0, 0, 0, 0]))
+
+        for col in range(self.grid_width):
+            line_coords = [col * self.cell_size, 0,
+                           col * self.cell_size, self.grid_height * self.cell_size]
+
+            main_batch.add(2, pyglet.gl.GL_LINES, None,
+                           ('v2i', line_coords),
+                           ('c3B', [0, 0, 0, 0, 0, 0]))
+
+        for row in range(self.grid_height):
+            for col in range(self.grid_width):
+                if row == 0 or row == self.grid_height - 1 or col == 0 or col == self.grid_width - 1:
+                    square_coords = [row * self.cell_size, col * self.cell_size,
+                                     row * self.cell_size, col * self.cell_size + self.cell_size,
+                                     row * self.cell_size + self.cell_size, col * self.cell_size,
+                                     row * self.cell_size + self.cell_size, col * self.cell_size + self.cell_size]
+
+                    main_batch.add_indexed(4, pyglet.gl.GL_TRIANGLES, None,
+                                           [0, 1, 2, 1, 2, 3],
+                                           ('v2i', square_coords),
+                                           ('c3B', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+
+        main_batch.draw()
+
+    def draw(self):
+        main_batch = pyglet.graphics.Batch()
+
+        square_coords = [self.head_x * self.cell_size, self.head_y * self.cell_size,
+                         self.head_x * self.cell_size, self.head_y * self.cell_size + self.cell_size,
+                         self.head_x * self.cell_size + self.cell_size, self.head_y * self.cell_size,
+                         self.head_x * self.cell_size + self.cell_size, self.head_y * self.cell_size + self.cell_size]
+
+        main_batch.add_indexed(4, pyglet.gl.GL_TRIANGLES, None,
+                               [0, 1, 2, 1, 2, 3],
+                               ('v2i', square_coords),
+                               ('c3B', [0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0]))
+
+        for (row, col) in self.snake:
+            square_coords = [row * self.cell_size, col * self.cell_size,
+                             row * self.cell_size, col * self.cell_size + self.cell_size,
+                             row * self.cell_size + self.cell_size, col * self.cell_size,
+                             row * self.cell_size + self.cell_size, col * self.cell_size + self.cell_size]
+
+            main_batch.add_indexed(4, pyglet.gl.GL_TRIANGLES, None,
+                                   [0, 1, 2, 1, 2, 3],
+                                   ('v2i', square_coords),
+                                   ('c3B', [0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0]))
+
+        square_coords = [self.food_x * self.cell_size, self.food_y * self.cell_size,
+                         self.food_x * self.cell_size, self.food_y * self.cell_size + self.cell_size,
+                         self.food_x * self.cell_size + self.cell_size, self.food_y * self.cell_size,
+                         self.food_x * self.cell_size + self.cell_size, self.food_y * self.cell_size + self.cell_size]
+
+        main_batch.add_indexed(4, pyglet.gl.GL_TRIANGLES, None,
+                               [0, 1, 2, 1, 2, 3],
+                               ('v2i', square_coords),
+                               ('c3B', [0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255]))
+
+        main_batch.draw()
+
+    def set_direction(self, direction):
+        if self.updated:
+            correct_move = False
+            if self.direction == "LEFT" and direction is not "RIGHT":
+                correct_move = True
+            elif self.direction == "RIGHT" and direction is not "LEFT":
+                correct_move = True
+            elif self.direction == "UP" and direction is not "DOWN":
+                correct_move = True
+            elif self.direction == "DOWN" and direction is not "UP":
+                correct_move = True
+
+            if correct_move:
+                self.direction = direction
+                self.updated = False
+
+    def run_rules(self):
+        new_head = tuple([sum(x) for x in zip(self.head, DIRECTIONS[self.direction])])
+
+        # Check for collisions
+        if new_head[0] <= 0 or new_head[0] >= self.grid_width - 1 or new_head[1] <= 0 or new_head[1] >= self.grid_height - 1:
+            return True
+
+        if new_head in self.snake[:-1]:
+            return True
+
+        # Check for food
+        if new_head == self.food:
+            self.snake = [self.head] + self.snake
+            self.food = self.generate_coords()
+            self.score += 1
+        else:
+            old_snake = self.snake
+            self.snake = [self.head]
+            for i in range(1, len(old_snake)):
+                self.snake.append(old_snake[i - 1])
+
+        self.head = new_head
+        self.updated = True
+
+        return False
+
+    def generate_coords(self):
+        x = random.randint(2, self.grid_width - 2)
+        y = random.randint(2, self.grid_height - 2)
+
+        while (x, y) in self.head or (x, y) in self.snake:
+            x = random.randint(2, self.grid_width - 2)
+            y = random.randint(2, self.grid_height - 2)
+
+        return x, y
+
+    def reset(self):
+        self.head = [int(self.grid_width / 2), int(self.grid_height / 2)]
+        self.snake = [(int(self.grid_width / 2), int(self.grid_height / 2) - 1),
+                      (int(self.grid_width / 2), int(self.grid_height / 2) - 2),
+                      (int(self.grid_width / 2), int(self.grid_height / 2) - 3)]
+        self.direction = "UP"
+        self.food = self.generate_coords()
